6907dace76386ea4ff121aff5ce965d90f5fb457
aditp0002 <adit@foxontherun.nl>
2020-12-15 19:51:59 +0100

    Initial commit, added index.php, favicon and assets folder

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="icon" type="image/png" href="favicon.png" />
+	
+	    <meta name="description" content="The best DJ in the world.">
+        <meta name="keywords" content="DJ, Best DJ, #1 DJ, Drain Gang">
+
+        <title>Best DJ</title>
+    </head>
+
+    <body>
+        <div id="content">
+
+            <div class="showcase">
+            
+                <div class="img-placeholder">
+                </div>
+
+                <div class="info-placeholder">
+
+                    <div class="head-title">
+                    </div>
+
+                    
+                    <div class="info">
+                        <div class="socials">
+                            <div class="icons">
+                                <a href="https://open.spotify.com/artist/6hG0VsXXlD10l60TqiIHIX?si=akW-AyvvQImshZ_U14HrlQ">
+                                    <img class="icons" src="assets/img/spot.png" alt="Spotify">
+                                </a>
+                            </div>
+                            <div class="icons">
+                                <a href="https://www.instagram.com/thaiboygoon/">
+                                    <img class="icons" src="assets/img/insta.png" alt="Instagram">
+                                </a>
+                            </div>
+                            <div class="icons">
+                                <a href="https://year0001.com/">
+                                    <img class="icons" src="assets/img/y1.png" alt="Instagram">
+                                </a>
+                            </div>
+
+                        </div>
+
+                        <div class="playlist">
+                            <h2>My Fantasy World</h2><br>
+                            <p> 
+                                01. Intro<br>
+                                02. Fullmoon<br>
+                                03. Deutsche Bank<br>
+                                04. Icebox<br>
+                                05. Fantasy World
+                            </p>
+                        </div>
+
+                        <div class="tiger">
+                        </div>
+                    </div>
+                
+                </div>
+            </div>
+
+            <div class="credits">
+                <p>created by <a href="https://www.instagram.com/aditvdam/">[@aditvdam]</a></p>
+            </div>
+
+            <div class="spotify-player">
+                <iframe class="player" src="https://open.spotify.com/embed/track/1T9YAGcs3K89PDZ3kD3YrG" width="600" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            </div>
+    </body>
+</html>
